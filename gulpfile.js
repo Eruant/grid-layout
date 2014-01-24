fde06ff92866a0851414636af0a18c9091bca5e3
Eruant <info@littleball.co.uk>
2014-01-24 12:15:39 +0000

    added script tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,61 @@
-var gulp = require('gulp'),
+/**
+ * gulpfile.js
+ *
+ * This file is used to set up the gulp task runner
+ */
+var browserify = require('gulp-browserify'),
   bump = require('gulp-bump'),
-  git = require('gulp-git');
+  concat = require('gulp-concat'),
+  git = require('gulp-git'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  gulp = require('gulp');
 
 gulp.task('default', function () {
   gulp.run('compile');
 });
 
 gulp.task('compile', function () {
-  gulp.run();
+  gulp.run('scripts');
 });
 
+/**
+ * scripts - used to lint and build JavaScript files
+ */
+gulp.task('scripts', function () {
+
+  // run jshint on files
+  gulp.src('./src/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'));
+
+  gulp.src(['./src/js/root.js'])
+    .pipe(browserify({
+      debug: true  
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(concat('bundle.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+
+});
+
+/**
+ * save - used to save the files and push them to remote git repository
+ */
 gulp.task('save', ['compile'], function () {
   if (!gulp.env.m) {
     console.error('ABORTING: Commit message must be set (--m "<message>")');
     return;
   }
 
+  // update the version number
   gulp.src('./package.json')
     .pipe(bump())
     .pipe(gulp.dest('./'));
 
+  // push changes to remote git repository
   gulp.src('./')
     .pipe(git.pull('origin', 'master'))
     .pipe(git.add())
---
+++ src/js/root.js
@@ -0,0 +1,3 @@
+var testObj = {
+  "name": "a test object"
+};
